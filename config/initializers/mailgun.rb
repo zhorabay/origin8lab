8746698707fc5bf886b49d9ca82360b57c51497d
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-04-27 19:25:41 +0300

    Add a mailgun domain

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,4 +1,4 @@
 Mailgun.configure do |config|
   config.api_key = '504dfff94395ef08f6653c2a52b3b5e9-2175ccc2-6b1c9ad1'
-  config.domain = 'sandbox123.mailgun.org'
+  config.domain = 'sandbox4ecd5ba090ce41f4ab733b80bba065df.mailgun.org'
 end
