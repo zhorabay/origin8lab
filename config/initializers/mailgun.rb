55b53ce4d9d791dc67126c738d7d9883ccf3dd72
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-05-10 23:31:05 +0300

    Update mailgun

--- config/initializers/mailgun.rb
+++ config/initializers/mailgun.rb
@@ -1,4 +1,4 @@
 Mailgun.configure do |config|
-  config.api_key = '504dfff94395ef08f6653c2a52b3b5e9-2175ccc2-6b1c9ad1'
+  config.api_key = '6202d063e087439c69d05b090afc5663-ed54d65c-bff4fa62'
   config.domain = 'sandbox4ecd5ba090ce41f4ab733b80bba065df.mailgun.org'
 end
