3657ecd09f5e2aada34a07d9e35fa57f5bb6cc46
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-05-11 17:31:04 +0300

    Make cors routes separate

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,56 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://origin8lab.com' # Add your frontend URLs
+    origins 'https://origin8lab.com' # Replace with your frontend URL
 
-    resource '*', # Adjust this to match your actual API endpoints
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options],
-      credentials: true
+    resource '/api/v1/*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options],
+             credentials: true
+  end
+
+  allow do
+    origins 'https://origin8lab.com' # Replace with your frontend URL
+
+    resource '/login',
+             headers: :any,
+             methods: [:post], # Adjust with the method used for login
+             credentials: true
+  end
+
+  allow do
+    origins 'https://origin8lab.com' # Replace with your frontend URL
+
+    resource '/signup',
+             headers: :any,
+             methods: [:post], # Adjust with the method used for signup
+             credentials: true
+  end
+
+  allow do
+    origins 'https://origin8lab.com' # Replace with your frontend URL
+
+    resource '/logout',
+             headers: :any,
+             methods: [:delete], # Adjust with the method used for logout
+             credentials: true
+  end
+
+  allow do
+    origins 'https://origin8lab.com' # Replace with your frontend URL
+
+    resource '/search',
+             headers: :any,
+             methods: [:get], # Adjust with the method used for logout
+             credentials: true
+  end
+
+  allow do
+    origins 'https://origin8lab.com' # Replace with your frontend URL
+
+    resource '/payments/success',
+             headers: :any,
+             methods: [:post], # Adjust with the method used for logout
+             credentials: true
   end
 end
