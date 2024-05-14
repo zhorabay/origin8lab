7fdfd0bd4b981a2762abaeb826eff4632847bcf4
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-05-15 01:55:30 +0300

    Add password to the mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(api_user)
     @api_user = api_user
+    @password = password
     mail(to: @api_user.email, subject: 'Welcome to Origin8lab!')
   end
 end
