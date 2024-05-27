c46998fa89010569777415c62be57b6e4d6558d9
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-05-27 10:58:58 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,63 +4,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://origin8lab.com' # Replace with your frontend URL
 
-    resource '/api/v1/*',
+    resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options],
-             credentials: true
-  end
-
-  allow do
-    origins 'https://origin8lab.com' # Replace with your frontend URL
-
-    resource '/login',
-             headers: :any,
-             methods: [:post], # Adjust with the method used for login
-             credentials: true
-  end
-
-  allow do
-    origins 'https://origin8lab.com' # Replace with your frontend URL
-
-    resource '/signup',
-             headers: :any,
-             methods: [:post], # Adjust with the method used for signup
-             credentials: true
-  end
-
-  allow do
-    origins 'https://origin8lab.com' # Replace with your frontend URL
-
-    resource '/registration',
-             headers: :any,
-             methods: [:post], # Adjust with the method used for signup
-             credentials: true
-  end
-
-  allow do
-    origins 'https://origin8lab.com' # Replace with your frontend URL
-
-    resource '/logout',
-             headers: :any,
-             methods: [:delete], # Adjust with the method used for logout
-             credentials: true
-  end
-
-  allow do
-    origins 'https://origin8lab.com' # Replace with your frontend URL
-
-    resource '/search',
-             headers: :any,
-             methods: [:get], # Adjust with the method used for logout
-             credentials: true
-  end
-
-  allow do
-    origins 'https://origin8lab.com' # Replace with your frontend URL
-
-    resource '/payments/success',
-             headers: :any,
-             methods: [:post, :get], # Adjust with the method used for logout
              credentials: true
   end
 end
