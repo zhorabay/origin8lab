a0e7863a47e34b5ba530b4cf22574f5429009652
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-05-29 16:32:11 +0300

    Finish testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Replace with your frontend URL
+    origins 'https://origin8lab.com' # Replace with your frontend URL
 
     resource '*',
              headers: :any,
