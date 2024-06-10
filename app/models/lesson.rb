7bc3ea6e71335289904c9df203847226896d2459
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-06-11 00:33:20 +0300

    Update the validation

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,5 +3,5 @@ class Lesson < ApplicationRecord
   has_many :comments
   has_many_attached :files
 
-  validates :google_form_link, url: true, allow_blank: true
+  validates :google_form_link, format: { with: URI::DEFAULT_PARSER.make_regexp, message: 'must be a valid URL' }, allow_blank: true
 end
