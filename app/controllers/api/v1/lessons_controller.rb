aad145e9b2e5703884a32102627872cc25e4da3e
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-06-11 00:37:09 +0300

    Make an able to add multiple links

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -72,11 +72,12 @@ class Api::V1::LessonsController < ApplicationController
   def lesson_params_with_conversions
     lesson_params.tap do |lp|
       lp[:course_module_id] = lp[:course_module_id].to_i if lp[:course_module_id].present?
+      lp[:google_form_links] = lp[:google_form_links].reject(&:blank?) if lp[:google_form_links].present?
     end
   end
 
   def lesson_params
-    params.require(:lesson).permit(:course_module_id, :title, :description, :google_form_link, files: [])
+    params.require(:lesson).permit(:course_module_id, :title, :description, google_form_links: [], files: [])
   end
 
   def render_lessons_with_files(lessons)
@@ -110,8 +111,7 @@ class Api::V1::LessonsController < ApplicationController
           filename: file.filename.to_s,
           content_type: file.content_type
         }
-      end,
-      google_form_link: lesson.google_form_link
+      end
     )
     lesson_attributes
   end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,5 +3,17 @@ class Lesson < ApplicationRecord
   has_many :comments
   has_many_attached :files
 
-  validates :google_form_link, format: { with: URI::DEFAULT_PARSER.make_regexp, message: 'must be a valid URL' }, allow_blank: true
+  serialize :google_form_links, Array
+
+  validate :valid_google_form_links
+
+  private
+
+  def valid_google_form_links
+    google_form_links.each do |link|
+      unless link =~ URI::DEFAULT_PARSER.make_regexp
+        errors.add(:google_form_links, "#{link} is not a valid URL")
+      end
+    end
+  end
 end
