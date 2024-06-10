e4c6c185640362cac0eed83a274a46dfa869bb10
Assem Zhorabay <assem.zhorabay@gmail.com>
2024-06-11 01:45:38 +0300

    Fix errors

--- app/controllers/api/v1/lessons_controller.rb
+++ app/controllers/api/v1/lessons_controller.rb
@@ -72,11 +72,12 @@ class Api::V1::LessonsController < ApplicationController
   def lesson_params_with_conversions
     lesson_params.tap do |lp|
       lp[:course_module_id] = lp[:course_module_id].to_i if lp[:course_module_id].present?
+      lp[:google_form_links] = lp[:google_form_links].reject(&:blank?)
     end
   end
 
   def lesson_params
-    params.require(:lesson).permit(:course_module_id, :title, :description, files: [])
+    params.require(:lesson).permit(:course_module_id, :title, :description, google_form_links: [], files: [])
   end
 
   def render_lessons_with_files(lessons)
